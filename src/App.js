e691b164e03067f3e5343145ff93bf685743d778
honghyeong <scarletgold0601@gmail.com>
2021-10-17 02:29:35 +0900

    feat: chapter3

--- src/App.js
+++ src/App.js
@@ -1,51 +1,22 @@
-import PropTypes from "prop-types";
+import React from "react";
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I love {name}</h2>
-      <img alt={name} src={picture} />
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
 
-const foodList = [
-  {
-    name: "Kimchi",
-    image:
-      "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-  },
-  {
-    name: "bulgogi",
-    image:
-      "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM.jpg",
-  },
-  {
-    name: "kimbap",
-    image:
-      "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM.jpg",
-  },
-  {
-    name: "samgyetang",
-    image:
-      "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
-  },
-];
+  render() {
+    const { isLoading } = this.state;
 
-function App() {
-  return (
-    <div className="App">
-      {foodList.map((dish) => (
-        <Food name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
 
 export default App;
