862bf270586e23c03e18c7f37609fcf41be97d46
honghyeong <scarletgold0601@gmail.com>
2021-10-18 23:38:19 +0900

    feat: chapter4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -6,16 +8,41 @@ class App extends React.Component {
     movies: [],
   };
 
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
 
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => {
+              return (
+                <Movie
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.poster}
+                />
+              );
+            })}
+      </div>
+    );
   }
 }
 
